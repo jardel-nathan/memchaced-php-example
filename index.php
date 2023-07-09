241fa80b4bb770f0152b28053aedaa7edd8dfaaa
Jardel Nathan <jardelnathan22@hotmail.com>
2023-07-09 16:41:06 -0300

    Update tittle

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 # Memcached Example
 
 echo "<div>
-<h1>Memcached</h1>
+<h1>Memcached PHP - Example</h1>
 <p>Memcached is a general-purpose distributed memory caching system. It is often used to speed up dynamic database-driven websites by caching data and objects in RAM to reduce the number of times an external data source (such as a database or API) must be read.</p>
 <hr>
 </div>";
